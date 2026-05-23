aaeff5b388e2b1bee6b52b003b2cecddec77ff98
DavidLinoe <davideduardolino@outlook.com>
2026-05-23 12:35:11 -0300

    feat: separando as configuracoes do modulo para gerar as migrations

---
+++ src/database/config/data-source.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [__dirname + '/../models/*.ts'],
+  migrations: [__dirname + '/../migrations/*.ts'],
+});
--- src/database/config/database.module.ts
+++ src/database/config/database.module.ts
@@ -1,18 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppDataSource } from './data-source';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'enterprise_hub_nest',
-      entities: [__dirname + '/../models/*.ts'],
-      synchronize: true,
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot(AppDataSource.options)],
 })
 export class DatabaseModule {}
