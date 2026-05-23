1811b9b242d47af09344ede2cf1cb9277203d419
DavidLinoe <davideduardolino@outlook.com>
2026-05-23 13:58:50 -0300

    feat: dot env

--- src/features/auth/auth.module.ts
+++ src/features/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthRepository } from './repositories/auth.repository';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
+import { User } from 'src/database/models/user';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
   providers: [AuthService, AuthRepository],
 })
