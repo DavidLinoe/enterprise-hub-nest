3736ad0ad98032cc35875c338a9d44981d1c92af
DavidLinoe <davideduardolino@outlook.com>
2026-05-23 19:02:25 -0300

    feat: criando camada de repositorio

---
+++ src/features/users/repositories/users.repository.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/database/models/user';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersRepository {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async find(userId: string): Promise<[User[], number]> {
+    return await this.usersRepository.findAndCount({
+      where: { id: userId },
+    });
+  }
+
+  async create(user: Partial<User>): Promise<User> {
+    const entity = this.usersRepository.create(user);
+    return await this.usersRepository.save(entity);
+  }
+
+  async update(user: Partial<User>): Promise<User | null> {
+    if (!user.id) {
+      return null;
+    }
+    return await this.usersRepository.update(user.id, user).then(() => {
+      return this.usersRepository.findOne({ where: { id: user.id } });
+    });
+  }
+
+  async delete(userId: string): Promise<User | null> {
+    const entity = await this.usersRepository.findOne({
+      where: { id: userId },
+    });
+    if (!entity) {
+      return null;
+    }
+    return await this.usersRepository.remove(entity);
+  }
+}
