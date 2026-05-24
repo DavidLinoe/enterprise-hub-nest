f630e47fd6507012fc3b9835374bebc4179a0cb4
DavidLinoe <davideduardolino@outlook.com>
2026-05-23 22:35:32 -0300

    feat: importando jwt service e configurando chave

--- src/features/auth/auth.module.ts
+++ src/features/auth/auth.module.ts
@@ -4,9 +4,17 @@ import { AuthRepository } from './repositories/auth.repository';
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { User } from 'src/database/models/user';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, AuthRepository],
 })
