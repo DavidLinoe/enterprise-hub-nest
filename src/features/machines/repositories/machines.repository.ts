b3d2582de8e2bf8f84fb0d1603df5e05d37c9e8d
DavidLinoe <davideduardolino@outlook.com>
2026-05-23 22:26:09 -0300

    feat: adicionando camada de repositorio

---
+++ src/features/machines/repositories/machines.repository.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Machine } from 'src/database/models/machine';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class MachinesRepository {
+  constructor(
+    @InjectRepository(Machine)
+    private machinesRepository: Repository<Machine>,
+  ) {}
+
+  async find(machinesId: string): Promise<[Machine[], number]> {
+    return await this.machinesRepository.findAndCount({
+      where: { id: machinesId },
+    });
+  }
+
+  async create(machine: Partial<Machine>): Promise<Machine> {
+    const entity = this.machinesRepository.create(machine);
+    return await this.machinesRepository.save(entity);
+  }
+
+  async update(machine: Partial<Machine>): Promise<Machine | null> {
+    if (!machine.id) {
+      return null;
+    }
+    return await this.machinesRepository
+      .update(machine.id, machine)
+      .then(() => {
+        return this.machinesRepository.findOne({ where: { id: machine.id } });
+      });
+  }
+
+  async delete(machinesId: string): Promise<Machine | null> {
+    const entity = await this.machinesRepository.findOne({
+      where: { id: machinesId },
+    });
+    if (!entity) {
+      return null;
+    }
+    return await this.machinesRepository.remove(entity);
+  }
+}
