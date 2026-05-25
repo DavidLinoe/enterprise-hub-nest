c95df16a3d351af2c9c7dfbf1fab1a4719c92ab2
DavidLinoe <davideduardolino@outlook.com>
2026-05-25 12:15:00 -0300

    fix: campo de get

--- src/features/users/dtos/find.dto.ts
+++ src/features/users/dtos/find.dto.ts
@@ -3,8 +3,8 @@ import { ApiProperty } from '@nestjs/swagger';
 export class FindUsersDto {
   @ApiProperty({
     required: true,
-    description: 'User ID to filter companies',
+    description: 'Company ID to filter users',
     example: '495b2416-05d4-4e4f-9d0f-01a8d4a73ff0',
   })
-  userId!: string;
+  companyId!: string;
 }
